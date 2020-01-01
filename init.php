4db1950e60f9f180a95930189aad3e19fe2f30c4
BlockChyp SDK Builder <devops@blockchyp.com>
2020-01-01 17:52:07 +0000

    fixed init.php issue

--- init.php
+++ init.php
@@ -1,38 +1,3 @@
 <?php
 
-require(dirname(__FILE__) . '/lib/BlockChypClient.php');
-
-require(dirname(__FILE__) . '/lib/model/ReceiptSuggestions.php');
-require(dirname(__FILE__) . '/lib/model/Acknowledgement.php');
-require(dirname(__FILE__) . '/lib/model/PingRequest.php');
-require(dirname(__FILE__) . '/lib/model/PingResponse.php');
-require(dirname(__FILE__) . '/lib/model/MessageRequest.php');
-require(dirname(__FILE__) . '/lib/model/BooleanPromptRequest.php');
-require(dirname(__FILE__) . '/lib/model/TextPromptRequest.php');
-require(dirname(__FILE__) . '/lib/model/TextPromptResponse.php');
-require(dirname(__FILE__) . '/lib/model/BooleanPromptResponse.php');
-require(dirname(__FILE__) . '/lib/model/WhiteListedCard.php');
-require(dirname(__FILE__) . '/lib/model/AuthorizationRequest.php');
-require(dirname(__FILE__) . '/lib/model/BalanceRequest.php');
-require(dirname(__FILE__) . '/lib/model/BalanceResponse.php');
-require(dirname(__FILE__) . '/lib/model/RefundRequest.php');
-require(dirname(__FILE__) . '/lib/model/CaptureRequest.php');
-require(dirname(__FILE__) . '/lib/model/CaptureResponse.php');
-require(dirname(__FILE__) . '/lib/model/VoidRequest.php');
-require(dirname(__FILE__) . '/lib/model/VoidResponse.php');
-require(dirname(__FILE__) . '/lib/model/EnrollRequest.php');
-require(dirname(__FILE__) . '/lib/model/EnrollResponse.php');
-require(dirname(__FILE__) . '/lib/model/ClearTerminalRequest.php');
-require(dirname(__FILE__) . '/lib/model/GiftActivateRequest.php');
-require(dirname(__FILE__) . '/lib/model/GiftActivateResponse.php');
-require(dirname(__FILE__) . '/lib/model/CloseBatchRequest.php');
-require(dirname(__FILE__) . '/lib/model/CloseBatchResponse.php');
-require(dirname(__FILE__) . '/lib/model/TermsAndConditionsRequest.php');
-require(dirname(__FILE__) . '/lib/model/TermsAndConditionsResponse.php');
-require(dirname(__FILE__) . '/lib/model/AuthorizationResponse.php');
-require(dirname(__FILE__) . '/lib/model/TransactionDisplayDiscount.php');
-require(dirname(__FILE__) . '/lib/model/TransactionDisplayItem.php');
-require(dirname(__FILE__) . '/lib/model/TransactionDisplayTransaction.php');
-require(dirname(__FILE__) . '/lib/model/TransactionDisplayRequest.php');
-require(dirname(__FILE__) . '/lib/model/HeartbeatResponse.php');
-
+require(dirname(__FILE__) . '/lib/BlockChyp.php');
