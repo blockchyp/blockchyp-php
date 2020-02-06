10d6f30f5b9a0a29f3f1a92a9c089a6014d650ea
BlockChyp SDK Builder <devops@blockchyp.com>
2020-02-06 18:02:25 +0000

    Bump version to 2.0.1-rc4

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -30,7 +30,7 @@ class BlockChypClient
     const PROMPT_TYPE_CUSTOMER_NUMBER = 'customer-number';
     const PROMPT_TYPE_REWARDS_NUMBER = 'rewards-number';
 
-    const VERSION = '2.0.1-rc3';
+    const VERSION = '2.0.1-rc4';
 
     protected static $apiKey;
 
