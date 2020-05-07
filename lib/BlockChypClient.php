7bf4a530de8dd25464ce8e75391ff0816364603d
BlockChyp SDK Builder <devops@blockchyp.com>
2020-05-07 17:00:23 +0000

    Bump version to 2.3.3

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -40,7 +40,7 @@ class BlockChypClient
     const AVS_RESPONSE_POSTAL_CODE_MATCH = 'zip_match';
     const AVS_RESPONSE_ADDRESS_AND_POSTAL_CODE_MATCH = 'match';
 
-    const VERSION = '2.3.2';
+    const VERSION = '2.3.3';
 
     protected static $apiKey;
 
