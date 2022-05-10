65c064d21e40cff49431922b98a9f197707c9a07
BlockChyp SDK Builder <devops@blockchyp.com>
2022-05-10 06:14:24 +0000

    Bump version to 2.12.1

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -46,7 +46,7 @@ class BlockChypClient
     const CVM_TYPE_CDCVM = 'CDCVM';
     const CVM_TYPE_NO_CVM = 'No CVM';
 
-    const VERSION = '2.11.0';
+    const VERSION = '2.12.1';
 
     protected static $apiKey;
 
