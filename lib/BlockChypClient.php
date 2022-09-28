244523b2fe5f8ccbe3a9be4087f6dac950be4949
BlockChyp SDK Builder <devops@blockchyp.com>
2022-09-28 16:55:34 +0000

    Bump version to github-migration-test-4

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -53,7 +53,7 @@ class BlockChypClient
     const HEALTHCARE_TYPE_CLINIC = 'clinic';
     const HEALTHCARE_TYPE_DENTAL = 'dental';
 
-    const VERSION = 'github-migration-test-3';
+    const VERSION = 'github-migration-test-4';
 
     protected static $apiKey;
 
