8217ea3f91ee0e09f5a750109ead78587778d488
BlockChyp SDK Builder <devops@blockchyp.com>
2022-12-05 22:01:01 +0000

    Bump version to 2.14.3

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -53,7 +53,7 @@ class BlockChypClient
     const HEALTHCARE_TYPE_CLINIC = 'clinic';
     const HEALTHCARE_TYPE_DENTAL = 'dental';
 
-    const VERSION = '2.14.2';
+    const VERSION = '2.14.3';
 
     protected static $apiKey;
 
