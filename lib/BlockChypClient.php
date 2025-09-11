5a313322d1f20c52eb8545e34683ee0e7c87348d
BlockChyp SDK Builder <devops@blockchyp.com>
2025-09-11 21:35:46 +0000

    Merge branch 'master' into develop

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -57,7 +57,7 @@ class BlockChypClient
     const ROUNDING_MODE_NEAREST = 'nearest';
     const ROUNDING_MODE_DOWN = 'down';
   
-    const VERSION = '2.21.0';
+    const VERSION = '2.24.2';
 
     protected static $apiKey;
 
