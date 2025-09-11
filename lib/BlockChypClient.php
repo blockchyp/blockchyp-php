e06c524b279fd67cbee57111caa0647416905bd8
BlockChyp SDK Builder <devops@blockchyp.com>
2025-09-11 22:30:27 +0000

    Bump version to 2.24.4

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -57,7 +57,7 @@ class BlockChypClient
     const ROUNDING_MODE_NEAREST = 'nearest';
     const ROUNDING_MODE_DOWN = 'down';
   
-    const VERSION = '2.24.3';
+    const VERSION = '2.24.4';
 
     protected static $apiKey;
 
