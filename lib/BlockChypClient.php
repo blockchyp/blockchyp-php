c87837c95be6b62ff006f873f6e83995277370a9
BlockChyp SDK Builder <devops@blockchyp.com>
2025-10-06 21:03:43 +0000

    Merge pull request #342 from blockchyp/task/VIP-473
    Task/vip 473 - Added the updateToken route to the SDKs

--- lib/BlockChypClient.php
+++ lib/BlockChypClient.php
@@ -57,7 +57,7 @@ class BlockChypClient
     const ROUNDING_MODE_NEAREST = 'nearest';
     const ROUNDING_MODE_DOWN = 'down';
   
-    const VERSION = '2.24.4';
+    const VERSION = '2.24.5';
 
     protected static $apiKey;
 
